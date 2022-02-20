07b3f301b1990c10db8feecf95085e79a697b019
강성찬(kohen) <tjdcks1781@gmail.com>
2022-02-21 00:25:30 +0900

    [Nexters/FALCON#noissue] 구매시 deicmaled 값으로 교환되도록 버그픽스 (#9)
    * [Nexters/FALCON#noissue] 구매시 deicmaled 값으로 교환되도록 버그픽스
    
    - 파일사이즈로 인해 불필요 함수 제거
    
    * [Nexters/FALCON#noissue] bugfix
    
    * [Nexters/FALCON#noissue] bugfix

--- contracts/token/CookieFactory.sol
+++ contracts/token/CookieFactory.sol
@@ -13,8 +13,8 @@ contract CookieFactory is KIP17Full("CookiePang", "CKP"), Ownable {
     // Data & Type
     KIP7Token private tradeCurrency;
     uint256 private lastCookieId;
-    uint256 public mintingPriceForKlaytn;
-    uint256 public mintingPriceForHammer;
+    uint8 public mintingPriceForKlaytn;
+    uint8 public mintingPriceForHammer;
     address payable[] public coFounderAddress;
     mapping(uint256 => CookieInfo) private cookieInfos;
     mapping(uint256 => uint256) public cookieHammerPrices;
@@ -41,32 +41,20 @@ contract CookieFactory is KIP17Full("CookiePang", "CKP"), Ownable {
     }
     event CookieEvented(uint8 indexed eventStatus, uint256 indexed cookieId, address indexed from, uint256 hammerPrice, uint256 createdAt);
 
-    function withdraw() onlyOwner public {
-        uint256 percentage = getSharePercentage();
-        uint256 balanceForPercentage = SafeMath.div(address(this).balance, 100);
-        uint256 quota = SafeMath.mul(balanceForPercentage, percentage);
-        for (uint i = 0; i < coFounderAddress.length; i++) {
-            coFounderAddress[i].transfer(quota);
-        }
-    }
-    function withdrawHammer() onlyOwner public {
-        uint256 percentage = getSharePercentage();
-        uint256 hammerBalanceForPercentage = SafeMath.div(tradeCurrency.balanceOf(address(this)), 100);
-        uint256 quota = SafeMath.mul(hammerBalanceForPercentage, percentage);
-        for (uint i = 0; i < coFounderAddress.length; i++) {
-            tradeCurrency.safeTransfer(coFounderAddress[i], quota);
-        }
-    }
+    // Function
+    // NFT 거래에 사용할 KIP7 토큰 등록
     function setTradeCurrency(KIP7Token _tradeCurrency) onlyOwner external {
         tradeCurrency = _tradeCurrency;
     }
     function addCoFounder(address payable cofounder) onlyOwner public {
         coFounderAddress.push(cofounder);
     }
-    function setMintingPriceForKlaytn(uint256 _mintingPriceForKlaytn) onlyOwner external {
+    // 쿠키 발행에 필요한 klaytn 가격 등록
+    function setMintingPriceForKlaytn(uint8 _mintingPriceForKlaytn) onlyOwner external {
         mintingPriceForKlaytn = _mintingPriceForKlaytn;
     }
-    function setMintingPriceForHammer(uint256 _mintingPriceForHammer) onlyOwner external {
+    // 쿠키 발행에 필요한 Hammer 가격 등록
+    function setMintingPriceForHammer(uint8 _mintingPriceForHammer) onlyOwner external {
         mintingPriceForHammer = _mintingPriceForHammer;
     }
     function mintCookieByOwner(address creator, string memory _title, string memory _content, string memory _metaUrl, string memory _tag, uint256 _hammerPrice) onlyOwner public returns (uint256) {
@@ -102,7 +90,7 @@ contract CookieFactory is KIP17Full("CookiePang", "CKP"), Ownable {
         uint256 decimaledHammerPrice = mintingPriceForHammer * 1000000000000000000;
         require(tradeCurrency.balanceOf(msg.sender) >= decimaledHammerPrice, "Not Enough HammerCoin");
         if (mintingPriceForHammer > 0) {
-            tradeCurrency.transferFrom(msg.sender, address(this), mintingPriceForHammer);
+            tradeCurrency.transferFrom(msg.sender, address(this), decimaledHammerPrice);
         }
         uint256 cookieId = createCookie(_title, _content, _metaUrl, _tag, _hammerPrice);
         return cookieId;
@@ -115,15 +103,18 @@ contract CookieFactory is KIP17Full("CookiePang", "CKP"), Ownable {
     }
     function buyCookie(uint256 _cookieId) public {
         require(saleCookies[_cookieId], "Cookie Not On Sale");
-        uint256 _hammerPrice = cookieHammerPrices[_cookieId] * 1000000000000000000;
-        require(tradeCurrency.balanceOf(msg.sender) >= _hammerPrice, "Not Enough Currency");
+        uint256 hammerPrice = cookieHammerPrices[_cookieId];
+        uint256 decimaledHammerPrice = hammerPrice * 1000000000000000000;
+        require(tradeCurrency.balanceOf(msg.sender) >= decimaledHammerPrice, "Not Enough Currency");
         address buyer = msg.sender;
         address seller = ownerOf(_cookieId);
         this.transferFrom(seller, buyer, _cookieId);
-        tradeCurrency.transferFrom(buyer, seller, _hammerPrice);
+
+        tradeCurrency.safeTransferFrom(buyer, seller, decimaledHammerPrice);
+        // 다시 해당 컨트랙에 전달
         approve(address(this), _cookieId);
 
-        emit CookieEvented(uint8(EventStatus.BUY), _cookieId, msg.sender, _hammerPrice, now);
+        emit CookieEvented(uint8(EventStatus.BUY), _cookieId, msg.sender, hammerPrice, now);
     }
     function tokenURI(uint256 cookieId) public view returns (string memory) {
         require(_exists(cookieId), "KIP17Metadata: URI query for non exist token");
